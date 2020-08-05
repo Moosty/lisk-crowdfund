118216fe3305cc7d04fbb966a34ae4ab602b7db7
jurrem <jurre@moosty.com>
2020-08-05 20:57:09 +0200

    container stuff

--- src/components/Container.js
+++ src/components/Container.js
@@ -10,14 +10,14 @@ export const Container = (props) => {
       h-auto justify-center
 
     sm:bg-green-500
-      sm:m-4 sm:mx-auto 
+      sm:m-4 sm:mx-auto sm:justify-center
 
     md:bg-blue-500
-      md:m-8 md:mx-auto
+      md:m-8 md:mx-auto md:justify-center
 
     lg:bg-pink-500
       lg:flex lg:flex-row lg:flex-wrap
-      lg:m-8 lg:mx-auto
+      lg:m-8 lg:mx-auto lg:justify-center
 
     xl:bg-teal-500 xl:mx-auto
       xl:p-8
--- src/views/Wallet/index.js
+++ src/views/Wallet/index.js
@@ -7,19 +7,16 @@ export const Wallet = (props) => {
 
   return <div className="bg-white overflow-hidden">
   <TopBar />
- <Header
- title="My Wallet"
- subtitle="overview of all my activities"
+  <Header
+    title="My Wallet"
+    subtitle="overview of all my activities"
   />
   <div className="lg:mt-5 lg:w-10/12 mx-auto flex flex-col ">
 
     <SingleItem type="investment" src="/images/pexels-photo-3951901.jpeg" title="Crowdfundcampaign Title" subtitle="Project name" />
-
-
-  <SingleItem type="investment" src="/images/pexels-photo-1149601.jpeg" title="Best Sneakers in the world" subtitle="Aqua Project" />
-
+    <SingleItem type="investment" src="/images/pexels-photo-1149601.jpeg" title="Best Sneakers in the world" subtitle="Aqua Project" />
     <SingleItem type="investment" src="/images/pexels-photo-3951901.jpeg" title="Crowdfundcampaign Title" subtitle="Project name" />
 
   </div>
-  </div>;
+</div>;
 }
