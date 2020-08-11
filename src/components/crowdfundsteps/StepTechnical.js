f5ad651c7d6ec38710d0f98398e668b8b4fe8df9
jurrem <jurre@moosty.com>
2020-08-11 15:49:56 +0200

    crowdfundmodal months

--- src/components/crowdfundsteps/StepTechnical.js
+++ src/components/crowdfundsteps/StepTechnical.js
@@ -1,13 +1,13 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import TextField from "@material-ui/core/TextField";
 import InputAdornment from "@material-ui/core/InputAdornment";
 import reducer from "../../store/reducers";
 import withReducer from "../../store/withReducer";
 import { useDispatch, useSelector } from "react-redux";
 import * as Actions from "../../store/actions";
-import { InputSelect } from "../";
 import Tooltip from "@material-ui/core/Tooltip";
 import InfoIcon from "@material-ui/icons/Info";
+import MenuItem from "@material-ui/core/MenuItem";
 
 export const StepTechnical = withReducer(
   "stepTechnical",
@@ -16,9 +16,25 @@ export const StepTechnical = withReducer(
   const dispatch = useDispatch();
   const form = useSelector(({ blockchain }) => blockchain.crowdfund.createForm);
 
+  const [possibleVotingPeriods, setPossibleVotingPeriods] = useState([]);
+
   useEffect(() => {
     console.log(form);
   }, [form]);
+
+  useEffect(() => {
+    if (form.periods) {
+      const possiblePeriods = [];
+      for (let i = 1; i < form.periods; i++) {
+        if (form.periods % i === 0) {
+          possiblePeriods.push(i);
+        }
+      }
+      setPossibleVotingPeriods(possiblePeriods);
+    } else {
+      setPossibleVotingPeriods([]);
+    }
+  }, [form.periods]);
 
   return (
     <form
@@ -63,33 +79,37 @@ export const StepTechnical = withReducer(
         }}
         style={{ marginBottom: 12 }}
       />
-      <TextField
-        id="outlined-multiline-static"
-        label="Amount of votings"
-        variant="outlined"
-        fullWidth
-        type="number"
-        value={form.periods / form.votings}
-        onChange={(e) => {
-          dispatch(
-            Actions.updateCrowdfundForm({
-              votings: parseInt(form.periods / e.target.value, 10),
-            })
-          );
-        }}
-        style={{ marginBottom: 12 }}
-      />
-      <div className="flex flex-row mt-2">
-        <span>Donators can vote every </span>
-        <InputSelect />
-        <span>month(s).</span>
+      <div className="flex flex-row items-baseline">
+        <span className="mr-1">Donators can vote every </span>
+        <Tooltip
+          title="Involve your funders in your project & keep them up to date"
+          placement="top-start"
+        >
+          <span className="flex flex-row items-baseline ">
+            <TextField
+              id="outlined-select-currency"
+              select
+              label="Select"
+              value={form.voting}
+              onChange={(e) =>
+                dispatch(
+                  Actions.updateCrowdfundForm({ voting: e.target.value })
+                )
+              }
+              helperText="Please select"
+              variant="outlined"
+              style={{ margin: 12 }}
+            >
+              {possibleVotingPeriods.map((period) => (
+                <MenuItem key={period} value={period}>
+                  {period}
+                </MenuItem>
+              ))}
+            </TextField>
+          </span>
+        </Tooltip>
+        <span className="ml-1">month(s).</span>
       </div>
-      <Tooltip
-        title="Involve your funders in your project & keep them up to date"
-        placement="top-start"
-      >
-        <InfoIcon />
-      </Tooltip>
     </form>
   );
 });
