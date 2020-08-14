453e42f33d4876247de742d541102c534a3de990
jurrem <jurre@moosty.com>
2020-08-14 13:47:14 +0200

    footer toegevoegd inclusief logo

--- src/views/Overview/index.js
+++ src/views/Overview/index.js
@@ -1,16 +1,19 @@
-import React from 'react';
-import { TopBar, Header, CardsContainer } from '../../components' ;
+import React from "react";
 
+import { TopBar, Header, CardsContainer, Footer } from "../../components";
 
 export const Overview = (props) => {
-
-  return <div className="overflow-hidden">
-    <TopBar />
-    <Header
-    title="Lisk Crowd | A Regulated Crowdfund Platform"
-    subtitle="A Regulated Crowdfund Platform"
-    button1="Explore Projects"
-    button2="Start Crowdfund" />
-    <CardsContainer />
-  </div>;
-}
+  return (
+    <div className="overflow-hidden">
+      <TopBar />
+      <Header
+        title="Lisk Crowd | A Regulated Crowdfund Platform"
+        subtitle="A Regulated Crowdfund Platform"
+        button1="Explore Projects"
+        button2="Start Crowdfund"
+      />
+      <CardsContainer />
+      <Footer />
+    </div>
+  );
+};
