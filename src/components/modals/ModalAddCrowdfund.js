3f35b58c1b312dc6e5b341a769fc951526fb5c7d
jurrem <jurre@moosty.com>
2020-08-29 11:17:08 +0200

    backbutton detail

--- src/components/modals/ModalAddCrowdfund.js
+++ src/components/modals/ModalAddCrowdfund.js
@@ -1,14 +1,14 @@
-import { makeStyles } from "@material-ui/core/styles";
-import { useDispatch, useSelector } from "react-redux";
+import {makeStyles} from "@material-ui/core/styles";
+import {useDispatch, useSelector} from "react-redux";
 import Backdrop from "@material-ui/core/Backdrop";
 import Button from "@material-ui/core/Button";
 import Fade from "@material-ui/core/Fade";
 import Modal from "@material-ui/core/Modal";
 import React from "react";
-import { useHistory, useParams } from 'react-router-dom';
+import {useHistory, useParams} from 'react-router-dom';
 import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
 
-import { CrowdfundStepper, ProjectInfo } from "../index";
+import {CrowdfundStepper, ProjectInfo} from "../index";
 import * as Actions from "../../store/actions";
 import reducer from "../../store/reducers";
 import withReducer from "../../store/withReducer";
@@ -18,96 +18,97 @@ import IconButton from "@material-ui/core/IconButton";
 //* Modaladdcrowdfund -> contains Crowdfundstepper -> contains content-steps  *//
 
 const useStyles = makeStyles((theme) => ({
-  modal: {
-    width: "80%",
-    minHeight: "80%",
-    minWidth: "80%",
-    height: "80%",
-    margin: "auto",
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  paper: {
-    minHeight: "100%",
-    minWidth: "80%",
-    backgroundColor: theme.palette.background.paper,
+    modal: {
+        width: "80%",
+        minHeight: "80%",
+        minWidth: "80%",
+        height: "80%",
+        margin: "auto",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+    },
+    paper: {
+        minHeight: "100%",
+        minWidth: "80%",
+        backgroundColor: theme.palette.background.paper,
 
-    boxShadow: theme.shadows[5],
-    padding: theme.spacing(2, 4, 3),
-  },
-  sidewindow: {
-    minWidth: "25%",
-  },
+        boxShadow: theme.shadows[5],
+        padding: theme.spacing(2, 4, 3),
+    },
+    sidewindow: {
+        minWidth: "25%",
+    },
 }));
 
 export const ModalAddCrowdfund = withReducer(
-  "ModalAddCrowdfund",
-  reducer
+    "ModalAddCrowdfund",
+    reducer
 )((props) => {
-  const classes = useStyles();
-  const dispatch = useDispatch();
-  const history = useHistory();
-  const params = useParams();
+    const classes = useStyles();
+    const dispatch = useDispatch();
+    const history = useHistory();
+    const params = useParams();
 
-  const {wallet} = useSelector(({blockchain}) => blockchain);
-  const {open, type} = useSelector(({modal}) => modal);
-  return (
-    <div>
-      {!props.wallet && !params.publicKey && wallet.account && wallet.account.address && (
-        <Button
-          size="small"
-          variant="contained"
-          color="secondary"
-          onClick={() => dispatch(Actions.openModal("createCrowdfund"))}
-        >
-          Create Crowdfund
-        </Button>
-      )}
-      {!props.wallet && !params.publicKey && !wallet.account.address && (
-        <Button
-          size="small"
-          variant="contained"
-          color="secondary"
-          onClick={() => dispatch(Actions.openModal("signup"))}
-        >
-          Create Crowdfund
-        </Button>
-      )}
-      {(props.wallet || params.publicKey) && (
-        <IconButton onClick={() => history.push('/')}>
-          <ChevronLeftIcon/>
-        </IconButton>
+    const {wallet} = useSelector(({blockchain}) => blockchain);
+    const {open, type} = useSelector(({modal}) => modal);
+    return (
+        <div>
+            {!props.wallet && !params.publicKey && wallet.account && wallet.account.address && (
+                <Button
+                    size="small"
+                    variant="contained"
+                    color="secondary"
+                    onClick={() => dispatch(Actions.openModal("createCrowdfund"))}
+                >
+                    Create Crowdfund
+                </Button>
+            )}
+            {!props.wallet && !params.publicKey && !wallet.account.address && (
+                <Button
+                    size="small"
+                    variant="contained"
+                    color="secondary"
+                    onClick={() => dispatch(Actions.openModal("signup"))}
+                >
+                    Create Crowdfund
+                </Button>
+            )}
+            {(props.wallet || params.publicKey) && (
+                <IconButton style={{color: "#fff",}}
+                            onClick={() => history.push('/')}>
+                    <ChevronLeftIcon/>
+                </IconButton>
 
-      )}
-      <Modal
-        aria-labelledby="transition-modal-title"
-        aria-describedby="transition-modal-description"
-        className={classes.modal}
-        open={open && type === "createCrowdfund"}
-        onClose={() => dispatch(Actions.closeModal())}
-        closeAfterTransition
-        BackdropComponent={Backdrop}
-        BackdropProps={{
-          timeout: 500,
-        }}
-      >
-        <Fade in={open}>
-          <div className={classes.paper}>
-            <div className="flex flex-row">
-              <div className="flex flex-col w-3/4 max-w-2xl">
-                <h1 className="text-xl text-center sm:text-3xl lg:text-5l lg:leading-8 text-grey font-bold my-4">
-                  Create your own Crowdfund
-                </h1>
-                <CrowdfundStepper/>
-              </div>
-              <div className="max-w-md w-5/12 bg-gray-200 p-4 rounded-t-lg">
-                <ProjectInfo create/>
-              </div>
-            </div>
-          </div>
-        </Fade>
-      </Modal>
-    </div>
-  );
+            )}
+            <Modal
+                aria-labelledby="transition-modal-title"
+                aria-describedby="transition-modal-description"
+                className={classes.modal}
+                open={open && type === "createCrowdfund"}
+                onClose={() => dispatch(Actions.closeModal())}
+                closeAfterTransition
+                BackdropComponent={Backdrop}
+                BackdropProps={{
+                    timeout: 500,
+                }}
+            >
+                <Fade in={open}>
+                    <div className={classes.paper}>
+                        <div className="flex flex-row">
+                            <div className="flex flex-col w-3/4 max-w-2xl">
+                                <h1 className="text-xl text-center sm:text-3xl lg:text-5l lg:leading-8 text-grey font-bold my-4">
+                                    Create your own Crowdfund
+                                </h1>
+                                <CrowdfundStepper/>
+                            </div>
+                            <div className="max-w-md w-5/12 bg-gray-200 p-4 rounded-t-lg">
+                                <ProjectInfo create/>
+                            </div>
+                        </div>
+                    </div>
+                </Fade>
+            </Modal>
+        </div>
+    );
 });
--- src/views/Wallet/index.js
+++ src/views/Wallet/index.js
@@ -6,7 +6,7 @@ import { WalletOverview } from "app/components/containers";
 export const Wallet = (props) => {
   return (
     <div className="bg-white overflow-hidden">
-      <Header title="My Wallet" subtitle="overview of all my activities" />
+      <Header title="My Wallet" subtitle="overview of all my crowdfunds, projects & investments" />
       <div className="mb-4 lg:mt-5 lg:w-10/12 mx-auto flex flex-col lg:flex-row flex-wrap justify-start items-start ">
         {/*<AccordeonWallet />*/}
         <WalletOverview />
